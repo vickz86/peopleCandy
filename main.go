110d34f5aa5c6c0e496ccf1ac2dcd51ee0503eb7
victor chassaigne <vicchassaigne@hotmail.com>
2024-07-04 16:33:25 -0400

    add main loop logic in main

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	con "github.com/vickz86/peopleCandy/constructor"
 	data "github.com/vickz86/peopleCandy/data"
@@ -12,18 +13,43 @@ var Peoples = []data.People{
 	{
 		Name:    "victor",
 		Money:   100,
-		Candies: [3]int{2, 4, 6}},
+		Candies: [3]int{5, 6, 9}},
 	{
 
 		Name:    "jonathan",
 		Money:   200,
 		Candies: [3]int{1, 3, 4}},
+	{
+
+		Name:    "amelie",
+		Money:   300,
+		Candies: [3]int{0, 2, 1}},
 }
 
 //start main
 
 func main() {
-	Peoples = con.CreatePeople(Peoples)
-	fmt.Println(Peoples)
+	//print string
+	possibleToDo := "choose between\n 0 exit\n1 add person\n2 print people\n"
+	var toDo int
+
+	//loop action
+	for {
+		//print and get action
+		fmt.Print(possibleToDo)
+		fmt.Scan(&toDo)
+		//swictch what to do
+		switch toDo {
+		case 0:
+			os.Exit(1)
+		case 1:
+			Peoples = con.CreatePeople(Peoples)
+		case 2:
+			fmt.Println(Peoples)
+		}
+
+	}
 
 }
+
+/* TODO , package money , create switch */
